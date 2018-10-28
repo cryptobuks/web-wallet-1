9bcdb1f3cf7073e2fb112c5e9858bc09f184fe92
affankhan43 <affankhan627@gmail.com>
2018-10-29 01:05:12 +0500

    models

--- app/Http/Controllers/Api/Coincontroller.php
+++ app/Http/Controllers/Api/Coincontroller.php
@@ -209,6 +209,7 @@ class Coincontroller extends Controller
 				else{
 					if($request->coin == "KMD"){
 						$w_data = json_decode($request->withdrawal_data,true);
+						print_r($w_data);
 						for($i=0; $i < sizeof($w_data); $i++){ 
 							if(isset($w_data[$i]['amount']) && isset($w_data[$i]['address'])){
 								$vout_data[] = array("address"=>$w_data[$i]['address'],"amount"=>$w_data[$i]['amount']);
