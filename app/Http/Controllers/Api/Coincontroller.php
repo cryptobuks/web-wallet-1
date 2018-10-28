b7732b5e0065195f9dd2c8e02615f24ff21501c5
affankhan43 <affankhan627@gmail.com>
2018-10-29 00:52:50 +0500

    models

--- app/Http/Controllers/Api/Coincontroller.php
+++ app/Http/Controllers/Api/Coincontroller.php
@@ -185,6 +185,93 @@ class Coincontroller extends Controller
 			return response()->json(['success'=> false, 'message'=> 'Access Denied !']);
 		}
 	}
+
+	public function create_raw(Request $request){
+		$user = $this->jwtauth->parseToken()->authenticate();
+		if($user->is_verified == 1 && $user->id == 1){
+			$request->validate([
+				'coin' => 'required',
+				'withdraw_data'=>'required|json'
+			]);
+			$coin = $this->coin->where(['coin'=>$request->coin])->get(['id','coin','withdrawals']);
+			if($coin == "[]"){
+				return response()->json(['success'=> false, 'message'=> 'Coin Not Found']);
+			}
+			/*---- Main ---- */
+			else{
+				$coin_data = json_encode($coin);
+				$coin_data = substr($coin_data,1);
+				$coin_data = substr_replace($coin_data,"", -1);
+				$coin_data = (json_decode($coin_data,true));
+				if($coin_data['withdrawals'] == 0){
+					return response()->json(['success'=> false, 'message'=> 'withdrawals Disabled']);
+				}
+				else{
+					if($request->coin == "KMD"){
+						$w_data = json_decode($request->withdrawal_data,true);
+						for($i=0; $i < sizeof($w_data); $i++){ 
+							if(isset($w_data[$i]['amount']) && isset($w_data[$i]['address'])){
+								$vout_data[] = array("address"=>$w_data[$i]['address'],"amount"=>$w_data[$i]['amount']);
+							}
+							else{
+								return response()->json(['success'=>false,'message'=>'Data Missing..']);
+							}
+						}
+						if(isset($vout_data) && is_array($vout_data)){
+							$create_raw = new Curl();
+							$create_raw->post("http://127.0.0.1/komodo/komodo.php",array(
+								"msg"=>"createraw",
+								"key"=>"chow_getraw",
+								"coin"=>"kmd",
+								"tx_fees"=>0.0001,
+								"with_data"=>json_encode($vout_data)
+							));
+							if($create_raw->error){
+								return response()->json(['success'=>false,'message'=>'Network Error']);
+							}
+							else{
+								$json_raw_data = json_decode($create_raw->response,true);
+								if(isset($json_raw_data['raw_hash'])){
+									if($json_raw_data['raw_hash'] == false){
+										return response()->json(['success'=>false,'message'=>'Invalid Data']);
+									}
+									else{
+										return response()->json(['success'=>true,'raw_hash'=>$json_raw_data['raw_hash']]);
+									}
+								}
+								elseif(isset($json_raw_data['message'])){
+									return response()->json(['success'=>false,'message'=>'Balance Not Enough']);
+								}
+								else{
+									return response()->json(['success'=>false,'message'=>"Invalid Error"]);
+								}
+							}
+						}
+						$getaddress = new Curl();
+						$getaddress->post("http://127.0.0.1/komodo/komodo.php",array('msg' =>'balance_off' ,'key' =>'kmd_offline_blnc','coin'=>'kmd'));
+						if($getaddress->errorMessage){
+							return response()->json(['success'=>false,'message'=>'Coin network error']);
+						}
+						else{
+							$dataa = json_decode($getaddress->response,true);
+							if(isset($dataa['balnc'])){
+								return response()->json(['success'=>true, 'network'=>$request->coin,'balance'=>$dataa['balnc'],"wallet"=>"RC8dPTpoCX7iaibYyEPP3vqbXwPs1gNots"]);
+							}
+							else{
+								return response()->json(['success'=>false,'message'=>'network error']);
+							}
+						}
+					}
+					else{
+						return response()->json(['success'=> false, 'message'=> 'Access Denied !']);
+					}
+				}
+			}
+		}
+		else{
+			return response()->json(['success'=> false, 'message'=> 'Access Denied !']);
+		}
+	}
 	
 /*-END-*/				
 }
